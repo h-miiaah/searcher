75341ab66858d1d292b1d78bad0b4fac02601aa4
h-miiaah <haim.miiaah5@gmail.com>
2022-02-07 22:30:23 -0500

    completed deugging to render App js file properly

--- src/App.js
+++ src/App.js
@@ -1,14 +1,3 @@
-// import React from 'react';
-
-// const App = () => {
-//   return 
-//     <div>
-//         <h1>App</h1>
-//     </div>;
-// };
-
-// export default App;
-
 import React from 'react';
 
 const App = () => {
