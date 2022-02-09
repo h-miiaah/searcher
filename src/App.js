eb913b1402595a013e1ad9f71c9b4c517fa62f7d
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:35:10 -0500

    app js added routes component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const App = () => {
         <div className={darkTheme ? 'dark' : ''}>
             <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
                 <Navbar />
+                <Routes />
                 
             </div>
         </div>
