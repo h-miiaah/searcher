2ca010d078c6cf10f711ca05c9cf410c1beb11fb
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:53:51 -0500

    index js imported browserrouter and wrapped the app component in the router component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import './global.css';
 
@@ -8,4 +9,9 @@ import './global.css';
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <App />
+    </Router>,
+document.getElementById('root')
+);
