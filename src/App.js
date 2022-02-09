6b532150a91ce9ebd1d650e8bedcd9f6c3078788
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:26:36 -0500

    imported useState to app js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Navbar} from './components/Navbar';
 import {Footer} from './components/Footer';
 import {Routes} from './components/Routes';
 
 const App = () => {
+    
+
     return(
         <div>
             <h1>App</h1>
