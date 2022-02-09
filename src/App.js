b73bb625e642fbb1c389531eae8e5131d2c06c65
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:25:11 -0500

    imported components to app js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import {Navbar} from './components/Navbar';
+import {Footer} from './components/Footer';
+import {Routes} from './components/Routes';
 
 const App = () => {
     return(
