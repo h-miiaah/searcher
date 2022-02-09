b7b78794a63538ceccb68f224139d5e05d7aaa0d
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:34:49 -0500

    app js added navbar component

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const App = () => {
     return(
         <div className={darkTheme ? 'dark' : ''}>
             <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-                App
+                <Navbar />
+                
             </div>
         </div>
     )
