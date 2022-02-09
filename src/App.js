f5afcbaa123459c6afe244457a4d5fb43fcc5c0a
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 20:33:59 -0500

    app js updates

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
     return(
         <div className={darkTheme ? 'dark' : ''}>
-            <div className="bg-gray-100 dark:bg-gray-900">
+            <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
                 App
             </div>
         </div>
