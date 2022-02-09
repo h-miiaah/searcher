bd386873d31cf58696362d89b298d56fc8ce6ff4
h-miiaah <haim.miiaah5@gmail.com>
2022-02-08 21:03:05 -0500

    navabar jsx updated darktheme button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ export const Navbar = ({darkTheme, setDarkTheme}) => {
                     Searcher 🔍
                 </p>
             </Link>
-            <button type="button" onClick={() => setDarkTheme(!darkTheme)}>
+            <button type="button" onClick={() => setDarkTheme(!darkTheme)} className=" text-xl dark:bg-gray-50 dark:text-gray-900 bg-white border rounded-full px-2 py-1">
                 {darkTheme ? 'Light' : 'Dark'}
             </button>
         </div>
