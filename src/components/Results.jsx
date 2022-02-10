6466ca3d60cbc0815695982ec9e8fda62d4de092
h-miiaah <haim.miiaah5@gmail.com>
2022-02-09 22:48:49 -0500

    results jsx added const location

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -7,6 +7,7 @@ import { useResultContext } from '../contexts/ResultContextProvider';
 
 export const Results = () => {
   const {results, isLoading, getResults, searchTerm} = useResultContext();
+  const location = useLocation();
   
   return (
     <div>
