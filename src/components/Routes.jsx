50233140b844631551c2d020a4201595cdbb8cd4
h-miiaah <haim.miiaah5@gmail.com>
2022-02-09 21:41:43 -0500

    routes jsx added padding

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -4,7 +4,7 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 
 export const Routes = () => {
   return (
-    <div>
+    <div className="p-4">
         <h1>Routes</h1>
     </div>
   ) 
