335beec9dd9861cd7c01e7050e1e6d8658b4aa02
h-miiaah <haim.miiaah5@gmail.com>
2022-02-09 22:20:37 -0500

    resultcontextprovider js added functional component

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -2,3 +2,7 @@ import React, {createContext, useContext, useState} from 'React';
 
 const ResultContext = createContext();
 const baseURL = 'https://google-search3.p.rapidapi.com/api/v1';
+
+export const ResultContextProvider = () => {
+    
+}
