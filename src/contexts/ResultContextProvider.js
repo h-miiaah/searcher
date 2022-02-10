7c08dcc5be698b048001eee1362e215357be9293
h-miiaah <haim.miiaah5@gmail.com>
2022-02-09 22:21:15 -0500

    resultcontextprovider js added functional component and children from props

--- src/contexts/ResultContextProvider.js
+++ src/contexts/ResultContextProvider.js
@@ -3,6 +3,6 @@ import React, {createContext, useContext, useState} from 'React';
 const ResultContext = createContext();
 const baseURL = 'https://google-search3.p.rapidapi.com/api/v1';
 
-export const ResultContextProvider = () => {
-    
+export const ResultContextProvider = ({childern}) => {
+
 }
