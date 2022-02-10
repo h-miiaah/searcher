5483fbaea10fab47fe2c7564188a8dc66c2e4e42
h-miiaah <haim.miiaah5@gmail.com>
2022-02-09 21:42:16 -0500

    routes jsx added switch component

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -5,7 +5,9 @@ import {Switch, Route, Redirect} from 'react-router-dom';
 export const Routes = () => {
   return (
     <div className="p-4">
-        <h1>Routes</h1>
+        <Switch>
+          
+        </Switch>
     </div>
   ) 
 };
