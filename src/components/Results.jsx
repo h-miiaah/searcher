a7640c52c68883d956187364545be831baf4ac82
h-miiaah <haim.miiaah5@gmail.com>
2022-02-21 20:56:48 -0500

    results jsx  updated news

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -56,7 +56,7 @@ export const Results = () => {
     case '/news':
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
-          {results?.entries?.map(({links, id, source, title}) => (
+          {results?.news?.map(({links, id, source, title}) => (
             <div key={id} className="md:w-2/5 w-full">
               <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
                 <p className="text-lg dark:text-blue-300 text-blue-700">
