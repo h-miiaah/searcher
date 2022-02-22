61f922d5ec46a8a469c85005ee96406fa1f7a82e
h-miiaah <haim.miiaah5@gmail.com>
2022-02-21 20:54:39 -0500

    results jsx  updated news

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -26,7 +26,7 @@ export const Results = () => {
     case '/search':
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56">
-          {results?.map(({link, title}, index) => (
+          {results?.results?.map(({link, title}, index) => (
             <div key={index} className="md:w-2/5 w-full">
               <a href={link} target="_blank" rel="noreferrer">
                 <p className="text-sm">
@@ -43,7 +43,7 @@ export const Results = () => {
     case '/images':
       return (
         <div className="flex flex-wrap justify-center items-center">
-          {image_results?.map(({image, link: {href, title}}, index) => (
+          {results?.image_results?.map(({image, link: {href, title}}, index) => (
             <a className="sm:p-3 p-5" href={href} key={index} target="_blank" rel="noreferrer">
               <img src={image?.src} alt={title} loading="lazy" />
               <p className="w-36 break-words text-sm mt-2">
@@ -56,20 +56,20 @@ export const Results = () => {
     case '/news':
       return (
         <div className="flex flex-wrap justify-between space-y-6 sm:px-56 items-center">
-          {/* {news?.map(({links, id, source, title}) => (
+          {results?.entries?.map(({links, id, source, title}) => (
             <div key={id} className="md:w-2/5 w-full">
               <a href={links?.[0].href} target="_blank" rel="noreferrer" className="hover:underline">
                 <p className="text-lg dark:text-blue-300 text-blue-700">
                   {title}
                 </p>
                 <div className="flex gap-4">
-                  <a href={source?.href} target="_blank" rel="noreferrer">
+                  <a href={source?.href} target="_blank" rel="noreferrer" className="hover:underline hover:text-blue-300">
                     {source?.href}
                   </a>
                 </div>
               </a>
             </div>
-          ))} */}
+          ))}
         </div>
       );
     case '/videos':
