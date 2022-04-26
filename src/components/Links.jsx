6e9acba734dcacfdd5341e358a2d0c271f7313d2
h-miiaah <haim.miiaah5@gmail.com>
2022-04-25 23:29:11 -0400

    updated icons

--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const links = [
-  { url: '/search', text: '🔎 All' },
+  { url: '/search', text: '🔍 All' },
   { url: '/news', text: '📰 News' },
   { url: '/images', text: '📸 Images' },
   { url: '/videos', text: '📺 Videos' },
